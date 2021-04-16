db9d9ca0300043c26d247f8f23d0d387a49d712b
Ahmedsk <ahmedsk128@gmail.com>
2021-04-16 02:58:07 +0200

    paytm section optimize

--- resources/views/includes/student_cart/paytm_checkout.blade.php
+++ resources/views/includes/student_cart/paytm_checkout.blade.php
@@ -1,35 +1,35 @@
 @if(env('PAYTM_MERCHANT_ID') != "" &&  env('PAYTM_MERCHANT_KEY') != "" &&  env('PAYTM_ACTIVE') != "NO" && paytmRouteForBlade())
-{{--PAYTM--}}
+	<div class="card-header" style="margin-bottom: -11px; padding: 0px 10px;">
+		<form action="{{ route('paytm.payment') }}" method="POST" id="payTmForm">
+		    @csrf
+		    @if(Session::has('coupon'))
+		        <input type="hidden" name="amount" value="{{ PaytmPrice($total_price - couponDiscountPrice($coupon)) }}">
+		    @else
+		        <input type="hidden" name="amount" value="{{ PaytmPrice($total_price) }}">
+		    @endif
+		</form>
 
-<form action="{{ route('paytm.payment') }}" method="POST" id="payTmForm">
-    @csrf
-    @if(Session::has('coupon'))
-        <input type="hidden" name="amount" value="{{ PaytmPrice($total_price - couponDiscountPrice($coupon)) }}">
-    @else
-        <input type="hidden" name="amount" value="{{ PaytmPrice($total_price) }}">
-    @endif
-</form>
-
-<a href="javascript:void()" title="Pay via PayTM" onclick="paytmPay()">
-	<div class="card border-bottom-0 paytm-top">
-		<div class="card-header">
-			<div class="checkout-item d-flex align-items-center justify-content-between">
-				<span class="widget-title font-size-18 stripe-btn w-75 d-block text-center font-weight-bold m-auto">
-						<img src="{{ filePath('paytm.png') }}" height="25px"
-							width="80px" alt="Paytm">
-				</span>
+		<a href="javascript:void()" title="Pay via PayTM" onclick="paytmPay()">
+			<div class="card border-bottom-0 paytm-top">
+				<div class="card-header">
+					<div class="checkout-item d-flex align-items-center justify-content-between">
+						<span class="widget-title font-size-18 stripe-btn w-75 d-block text-center font-weight-bold m-auto"
+							style="background: #EEE; border: 1px solid #666;">
+								<img src="{{ filePath('paytm.png') }}" height="25px"
+									width="80px" alt="Paytm">
+						</span>
+					</div>
+				</div>
 			</div>
-		</div>
+		</a>
 	</div>
-</a>
 
-@section('js')
-	<script>
-		function paytmPay(){
-			$('#payTmForm').submit();
-		}
-	</script>
-@append
+	@section('js')
+		<script>
+			function paytmPay(){
+				$('#payTmForm').submit();
+			}
+		</script>
+	@append
 
-{{--PAYTM ends--}}
 @endif
